3f0dcf2ac8fc64013c98f8abad37e078790a16d9
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-14 11:56:31 +0200

    implement ApplicationDbContext class

---
+++ CommandCliApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CommandCliApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandCliApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
